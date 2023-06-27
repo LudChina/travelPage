527d7425929c161ba86d2732ec7d0f6e4d3da10a
Ludmila Kolodij <chinakolodij@gmail.com>
2023-06-27 16:56:38 -0300

    css. Falta corregir responsive

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ['btn__primary', 'btn__outline'];
 
-const SIZES = ['btn--medium', 'btn--large'];
+const SIZES = ['btn__medium', 'btn__large'];
 
 export const Button = ({
     children,
@@ -13,5 +13,15 @@ export const Button = ({
     buttonStyle,
     buttonSize
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+    return(
+        <Link to='/registro' className='btn_mobile'>
+            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+                {children}
+            </button>
+        </Link>
+    )
 };
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,45 +1,62 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import '../Components/NavBar.css';
 
 function NavBar() {
   
   const [click, setClick] = useState(false)
+  const [button, setButton] = useState(true)
 
   const handleClick = () => (!click);
   const closeMobileMenu = () => setClick(false)
 
+
+
+
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
    <>
      <nav className="navBar">
-        <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
+        <div className="navbar_container">
+            <Link to="/" className="navbar_logo">
                 TRIP
             </Link>
-            <div className="menu-icon" onClick={handleClick}>
+            <div className="menu_icon" onClick={handleClick}>
               <i className={click ?  'fa-solid fa-xmark' : 'fa-solid fa-bars'} />
             </div>
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-              <li className='nav-item'>
-                <Link to="/" className='nav-links' onClick={closeMobileMenu}>
+            <ul className={click ? 'nav_menu active' : 'nav_menu'}>
+              <li className='nav_item'>
+                <Link to="/" className='nav_links' onClick={closeMobileMenu}>
                   Home
                 </Link>
               </li>
-              <li className='nav-item'>
-                <Link to="/experiencias" className='nav-links' onClick={closeMobileMenu}>
+              <li className='nav_item'>
+                <Link to="/experiencias" className='nav_links' onClick={closeMobileMenu}>
                   Experiencias
                 </Link>
               </li>
-              <li className='nav-item'>
-                <Link to="/destinos" className='nav-links' onClick={closeMobileMenu}>
+              <li className='nav_item'>
+                <Link to="/destinos" className='nav_links' onClick={closeMobileMenu}>
                   Destinos
                 </Link>
               </li>
-              <li className='nav-item'>
-                <Link to="/registro" className='nav-links-mobile' onClick={closeMobileMenu}>
+              <li className='nav_item'>
+                <Link to="/registro" className='nav_links_mobile' onClick={closeMobileMenu}>
                   Registro
                 </Link>
               </li>
             </ul>
+            {button && <Button buttonStyle='btn__outline'>Registrarse</Button>}
         </div>
      </nav>
 
